0b848bbf69b14777036f15a8240342faf76c8389
gwonyejun <kyjuni1059@gmail.com>
2023-06-04 16:07:55 +0900

    기본적인 함수와 반복문 조건문 예제 작성 해보기

--- Program.cs
+++ Program.cs
@@ -4,14 +4,31 @@ class Program
 {
   static void Main()
   {
-    int number = 10;
+    int number = 10, test = 0;
     string name = "yejun";
     Console.WriteLine(number + name);
-    if(number == 10){
+    if (number == 10)
+    {
       Console.WriteLine('일');
       Console.WriteLine("일치합니다.");
-      
+      for (int i = 0; i < number; i++)
+      {
+        test += 3;
+      }
+      Console.Write(test+"\n");
+      Console.Write("함수를 불러 add한 값 : "+ AddTest(test, number)
+);
+
+    }
+    else
+    {
+      Console.Write("아닌경우");
     }
 
+  }
+
+  static int AddTest(int a, int b)
+  {
+    return a + b;
   }
 }
